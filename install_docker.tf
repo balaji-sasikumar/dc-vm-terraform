ed5b80ced70959955710d4769ba73f7029134574
balaji <balaji@theproindia.com>
2025-01-30 11:57:24 +0530

    feat: install docker in vm

--- install_docker.tf
+++ install_docker.tf
@@ -31,10 +31,10 @@ resource "null_resource" "install_docker" {
       "sudo apt-get update",
       "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo add-apt-repository -y \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
       "sudo apt-get update",
       "sudo apt-get install -y docker-ce",
-      "sudo usermod -aG docker ${USER}",
+      "sudo usermod -aG docker ${var.admin_username}",
       "sudo systemctl start docker",
       "sudo systemctl enable docker"
     ]
